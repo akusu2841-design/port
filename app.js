58188d04d41941b7e47665636fbcad58c53d6b55
akusu2841-design <akusu2841@gmail.com>
2026-06-11 19:58:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const backBtn = Document.getElementById("backBtn");
 backBtn.addEventListener("click", function()
                          {history.back();});
+
+const menuBtn = document.getElementById("menuBtn")
+const navLinks = document.getElementById("nav-links")
+
+navLinks.addEventListener("click", function(){
+menuBtn.classList.toggle("active")
+});
